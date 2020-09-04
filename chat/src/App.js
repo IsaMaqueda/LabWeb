5f0bdc79c978cce04e374a2945a88754742c4440
IsaMaqueda <isamqd@gmail.com>
2020-09-04 15:19:58 -0500

    chat

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ChatBot from "../react-simple-chatbot";
 
-function App() {
+
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
+    <ChatBot steps={[
+        {
+          id: '1',
+          message: 'You can add custom components',
+          trigger: '2',
+        },
+        {
+          id: '2',
+          user: true,
+          trigger: '3',
+        },
+        {
+         id: '3',
+         component: (
+           <div> {previousValue} </div>
+          ),
+         end: true,
+         },
+        ]}
+        /> 
   );
 }
-
-export default App;
